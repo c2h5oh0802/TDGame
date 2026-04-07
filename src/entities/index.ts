72987479f40213188d4fba907aa0bbb331355274
winson0802 <chen.winson123@gmail.com>
2026-04-08 03:18:59 +0800

    chore: remove unused TypeScript architecture, simplify to single-file game

--- src/entities/index.ts
+++
@@ -1,8 +0,0 @@
-export { Entity, Particle, FloatingText } from './Entity';
-export { Player } from './Player';
-export { Core } from './Core';
-export { Projectile } from './Projectile';
-export { Enemy } from './Enemy';
-export { Building, Turret, Wall, Reactor } from './Building';
-export { FriendlyMinion } from './FriendlyMinion';
-export { Worker } from './Worker';
